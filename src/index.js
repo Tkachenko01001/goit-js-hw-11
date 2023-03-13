64dce56f0118361042bcd8511b81416dd8d99d45
Tkachenko01001 <113258288+Tkachenko01001@users.noreply.github.com>
2023-03-13 22:13:05 +0200

    updt-hw-11

--- src/index.js
+++ src/index.js
@@ -14,10 +14,8 @@ const onSearchImg = (e) => {
   clearGallery();
   e.preventDefault();
   showImg(fetchImages(inputEl.value));
-  showSuccess(fetchImages(inputEl.value));
   page += 1
   btnEl.classList.remove('hidden');
-  
 }
 
 const onClickLoadMore = (e) => {
@@ -26,8 +24,8 @@ const onClickLoadMore = (e) => {
 }
 
 const renderImg = (array) => {
- const gallery = array.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
-  return `<div class="gallery__item">
+  const gallery = array.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+    return `<div class="gallery__item">
   <a class="next" href=${largeImageURL}><img src= ${webformatURL} alt="${tags}" loading="lazy" width="350" height="200px" /></a>
   <div class="info">
     <p class="info-item">
@@ -44,61 +42,54 @@ const renderImg = (array) => {
     </p>
   </div>
 </div>`
-})
-.join("")
+  })
+    .join("")
 
- listEl.insertAdjacentHTML('beforeend', gallery);
- let lightbox = new SimpleLightbox('.gallery a');
- let ias = new InfiniteAjaxScroll('.gallery', {
-  item: '.gallery__item',
-  next: onClickLoadMore,
-  pagination: '.load-more',
-});
+  listEl.insertAdjacentHTML('beforeend', gallery);
+  let lightbox = new SimpleLightbox('.gallery a');
+  let ias = new InfiniteAjaxScroll('.gallery', {
+    item: '.gallery__item',
+    next: onClickLoadMore,
+    pagination: '.load-more',
+  });
 }
 
 const showImg = async (response) => {
-  const array = await response;
-  const totalPages = 100 / limit;
 
-  if (array.hits.length > 0) {
-    return renderImg(array.hits);
-  }
+  try {
+    const array = await response;
+    const totalPages = 100 / limit;
 
-  if (page > totalPages) {
-    btnEl.classList.add('hidden');
-    return Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
-  }
-  
-  if (array.hits.length === 0) {
-    btnEl.classList.add('hidden');
-    return Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.")
-  }
-  
-  const { height: cardHeight } = document
-  .querySelector(".gallery")
-  .firstElementChild.getBoundingClientRect();
+    if (array.hits.length > 0) {
+      return renderImg(array.hits);
+    }
 
-  window.scrollBy({
-  top: cardHeight * 2,
-  behavior: "smooth",
-  });
+    if (page > totalPages) {
+      btnEl.classList.add('hidden');
+      return Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
+    }
 
-  lightbox.refresh()
+    if (array.hits.length === 0) {
+      btnEl.classList.add('hidden');
+      return Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.")
+    }
 
-}
+    const { height: cardHeight } = document
+      .querySelector(".gallery")
+      .firstElementChild.getBoundingClientRect();
+      window.scrollBy({
+      top: cardHeight * 2,
+      behavior: "smooth",
+    });
 
-const showSuccess = async (response) => {
-  const allResult = await response;
-
-  if (allResult.hits.length > 0) {
-    Notiflix.Notify.success(`Hooray! We found ${allResult.totalHits} images.`)
+    lightbox.refresh()
+  } catch (error) {
+    console.log(error);
   }
 }
 
 const clearGallery = () => {
-  btnEl.classList.toggle('hidden');
-  page = 1;
-  listEl.innerHTML = '';
+  btnEl.classList.toggle('hidden'); page = 1; listEl.innerHTML = '';
 }
 
 btnEl.addEventListener('click', onClickLoadMore);
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Notiflix from 'notiflix';
 import { page } from "../index";
 export const inputEl = document.querySelector('input')
 export const limit = 40;
@@ -7,17 +8,22 @@ const API_KEY = '34292431-ace5bd535228626ecae00652c'
 
 export const fetchImages = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}${API_KEY}`,{
-        params: {
-            q: `${inputEl.value}`,
-            orientation: 'horizontal',
-            image_type: 'photo',
-            safesearch: 'true',
-            per_page: limit,
-            page: page, 
-        }
+    const response = await axios.get(`${BASE_URL}${API_KEY}`, {
+      params: {
+        q: `${inputEl.value}`,
+        orientation: 'horizontal',
+        image_type: 'photo',
+        safesearch: 'true',
+        per_page: limit,
+        page: page,
+      }
     })
-    return response.data; 
+
+    if (page === 2) {
+      Notiflix.Notify.success(`Hooray! We found ${response.data.totalHits} images.`)
+    }
+
+    return response.data;
 
   } catch (error) {
     console.log(error);
